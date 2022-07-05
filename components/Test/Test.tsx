068be452c30ea196eb0bfd95e45dee6edc2a3c8c
Ilya <ilya.p@finanso.com>
2022-07-05 22:15:21 +0300

    add image declaration

--- components/Test/Test.tsx
+++ components/Test/Test.tsx
@@ -1,7 +1,13 @@
 import styles from './Test.module.scss'
+import Logo from '../../src/logo.png'
+import svgIcon from '../../src/svg.svg'
+import ArrowIcon from '../../src/assets/images/Arrow'
 
 export const Test = () => {
     return <div className={styles.test}>
         test
+        <img src={Logo} alt="" />
+        <img src={svgIcon} alt="" width={20} height={20} />
+        <ArrowIcon />
     </div>;
 }
--- declarations.d.ts
+++ declarations.d.ts
@@ -2,3 +2,7 @@ declare module '*.scss' {
   const classes: { readonly [key: string]: string }
   export default classes
 }
+
+declare module '*.png'
+declare module '*.svg'
+declare module '*.jpeg'
