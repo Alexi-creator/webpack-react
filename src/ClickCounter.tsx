2ab9943d40c97100d0f325635806a5a3b50cc619
ilya Pavlov <ilya.p@finanso.com>
2022-07-06 15:37:13 +0300

    test

--- src/ClickCounter.tsx
+++ src/ClickCounter.tsx
@@ -2,9 +2,12 @@ import { useState } from 'react'
 
 export const ClickCounter = () => {
   const [count, setCount] = useState(0)
+  const test = 'test2'
   return (
     <div>
-      <button onClick={() => setCount((c) => c + 1)}>Count {count}</button>
+      <button onClick={() => setCount((c) => c + 1)}>
+        Count {count} {test}
+      </button>
     </div>
   )
 }
