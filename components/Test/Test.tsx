4e79b83119aa30e1d212ebb1566274f76ef98bc8
Ilya <ilya.p@finanso.com>
2022-07-16 22:23:10 +0300

    fix space

--- components/Test/Test.tsx
+++ components/Test/Test.tsx
@@ -4,10 +4,12 @@ import svgIcon from '../../src/svg.svg'
 import ArrowIcon from '../../src/assets/images/Arrow'
 
 export const Test = () => {
-    return <div className={styles.test}>
-        test
-        <img src={Logo} alt="" />
-        <img src={svgIcon} alt="" width={20} height={20} />
-        <ArrowIcon />
-    </div>;
+  return (
+    <div className={styles.test}>
+      test
+      <img src={Logo} alt="" />
+      <img src={svgIcon} alt="" width={20} height={20} />
+      <ArrowIcon />
+    </div>
+  )
 }
