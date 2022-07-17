7879ecc35ad628765d2f50596fcd1c3c4336a73f
Ilya <ilya.p@finanso.com>
2022-07-18 00:16:06 +0300

    add cn

--- components/Test/Test.tsx
+++ components/Test/Test.tsx
@@ -2,6 +2,7 @@ import styles from './Test.module.scss'
 import Logo from '../../src/logo.png'
 import svgIcon from '../../src/svg.svg'
 import ArrowIcon from '../../src/assets/images/Arrow'
+import cn from 'classnames'
 
 export const Test = () => {
   return (
@@ -9,7 +10,8 @@ export const Test = () => {
       test
       <img src={Logo} alt="" />
       <img src={svgIcon} alt="" width={20} height={20} />
-      <ArrowIcon />
+      <ArrowIcon className={styles.arrow} height={20} width={20} fill="red" />
+      <div className={cn(styles.cn1, styles.cn2)}>cn test</div>
     </div>
   )
 }
