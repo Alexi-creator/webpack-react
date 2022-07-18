230567352c3e6c454602df933678133fdae0af52
Ilya <ilya.p@finanso.com>
2022-07-18 11:43:40 +0300

    test cn

--- components/Test/Test.tsx
+++ components/Test/Test.tsx
@@ -3,15 +3,25 @@ import Logo from '../../src/logo.png'
 import svgIcon from '../../src/svg.svg'
 import ArrowIcon from '../../src/assets/images/Arrow'
 import cn from 'classnames'
+import { useState } from 'react'
 
 export const Test = () => {
+  const [state, setState] = useState<boolean>(true)
+
   return (
     <div className={styles.test}>
       test
       <img src={Logo} alt="" />
       <img src={svgIcon} alt="" width={20} height={20} />
       <ArrowIcon className={styles.arrow} height={20} width={20} fill="red" />
-      <div className={cn(styles.cn1, styles.cn2)}>cn test</div>
+      <button
+        onClick={() => setState(!state)}
+        className={cn(styles.cn1, styles.cn2, {
+          [styles.test3]: state,
+        })}
+      >
+        cn test
+      </button>
     </div>
   )
 }
